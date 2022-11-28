e14903b19e2a7cc541be782c5caf38b2de986327
sakotiya <sakotiya2707@gmail.com>
2022-11-27 18:33:01 -0800

    Final act

--- bar_chart.js
+++ bar_chart.js
@@ -96,7 +96,7 @@ function buildCharts(sample)
       type: "bar",
       orientation: 'h',
       text: otulabels,// this creates the hovertext
-      marker: { color: "darkmagenta" }
+      marker: { color: "light blue" }
     }];
   
 
@@ -137,6 +137,53 @@ function buildCharts(sample)
 
   });
   
+  // D3: 3. Use Plotly to plot the data with the layout.
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+
+    // Create a variable that converts the washing frequency to a floating point number.
+    var washFre = parseFloat(result.wfreq);
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      domain: d3.select(optionChanged),
+      value: washFre,
+      type: "indicator",
+      mode: "gauge+number",
+      title: { text: "<b>Belly Button Wash Frequency</b><br> Number of Scrubs per Week" },
+      gauge: {
+        axis: { range: [0, 10], tickwidth: 1, tickcolor: "black" },
+        bar: { color: "black" },
+        steps: [
+          { range: [0, 2], color: "red" },
+          { range: [2, 4], color: "orange" },
+          { range: [4, 6], color: "yellow" },
+          { range: [6, 8], color: "lightgreen" },
+          { range: [8, 10], color: "darkgreen" }
+        ],
+        threshold: {
+          line: { color: "lime", width: 6 },
+          thickness: 1.0,
+          value: 10
+        }
+      }
+
+    }
+    ];
+
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = {
+      width: 500,
+      height: 400,
+
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+  });
+}; 
 
 
-};
+  
